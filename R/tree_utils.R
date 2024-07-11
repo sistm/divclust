cb498bd6ac6516e3b863a4a126baa3cccb7a8011
borishejblum <boris.hejblum@gmail.com>
2024-07-11 17:53:07 +0200

    auto spacing Rstudio updates committed

--- R/tree_utils.R
+++ R/tree_utils.R
@@ -1,13 +1,13 @@
 #' @title list_leaves
-#' @description 
-#' This function finds terminal clusters, their monothetic description and the terminal partition. 
-#' If the tree is complete, the terminal partition is the partition of singleton (one observation in each cluster) 
+#' @description
+#' This function finds terminal clusters, their monothetic description and the terminal partition.
+#' If the tree is complete, the terminal partition is the partition of singleton (one observation in each cluster)
 #' @param cluster the divclust object
-#' @return leaves a list with the list of observations of a cluster ($class) and its monthetic description ($path) 
+#' @return leaves a list with the list of observations of a cluster ($class) and its monthetic description ($path)
 #' @return classes a vector of integers indicating the cluster to which each observation is allocated.
 #' @keywords internal
 #' @export
-list_leaves <- function(cluster) { 
+list_leaves <- function(cluster) {
   l <- list()
   f <- fifo_create()
   #fifo_add(f, list(node = cluster$tree, path = ""))
@@ -16,7 +16,7 @@ list_leaves <- function(cluster) {
     z <- fifo_remove(f)
     node <- z$node
     path <- z$path
-    
+
     sentence <- make_path(node, path)
     path_l <- sentence$l
     path_r <- sentence$r
@@ -24,7 +24,7 @@ list_leaves <- function(cluster) {
     #path_l <- paste(path, sentence$l, sep=', ')
     #path_r <- paste(path, sentence$r, sep=', ')
     if (is.null(node$l$l)) {
-      l[[length(l) + 1]] <-  list(class = node$v$A_l, path = path_l) 
+      l[[length(l) + 1]] <-  list(class = node$v$A_l, path = path_l)
     } else {
       fifo_add(f, list(node = node$l, path = path_l))
     }
@@ -32,15 +32,15 @@ list_leaves <- function(cluster) {
       l[[length(l) + 1]]  <- list(class = node$v$A_l_c, path = path_r)
     } else {
       fifo_add(f, list(node = node$r, path = path_r))
-    }  
+    }
   }
-  c <- rep(0, length(l)) 
-  k <- 1 
-  for (i in l) { 
+  c <- rep(0, length(l))
+  k <- 1
+  for (i in l) {
     for (j in i$class) {
-      c[j] <- k 
-    } 
-    k <- k + 1 
+      c[j] <- k
+    }
+    k <- k + 1
   }
   return(list(leaves = l, classes = c))
 }
@@ -48,7 +48,7 @@ list_leaves <- function(cluster) {
 #' @title Builds two necessary conditions
 #' @description Builds the monothetic description (path) of the two clusters of a bipartition
 #' @param node current node
-#' @param path of the current node 
+#' @param path of the current node
 #' @return the path of the two clusters of the biparition of the current node (left and rigth)
 #' @keywords internal
 #' @export
@@ -63,7 +63,7 @@ make_path <- function(node,path)
     z <- node$v$cut_val$value$bipart
     z_comp <- node$v$cut_val$value$bipart_c
   }
-  
+
   if (is.null(path)) {
     j <- 1
     path_l <- list()
@@ -87,7 +87,7 @@ make_path <- function(node,path)
     }
     else j <- length(vec_type)+1
   }
-  
+
   path_l[[j]]<- list(type=node$v$cut_val$type,cut_ind=node$v$cut_ind,val = z)
   path_r[[j]] <- list(type=node$v$cut_val$type,cut_ind=node$v$cut_ind,val = z_comp)
   return(list(left = path_l, right = path_r))
@@ -96,21 +96,21 @@ make_path <- function(node,path)
 #' @title Description of the leaves
 #' @description Builds the monothetic description of the leaves
 #' @param leaves a list of leaves
-#' @param cluster the divclust object containing the tree 
+#' @param cluster the divclust object containing the tree
 #' @return The monothetic description of the leaves
 #' @keywords internal
 #' @export
-#' 
+#'
 
 make_description <- function(leaves,cluster)
-{  
+{
   mod_quali <- cluster$mod_quali
   cnames_quanti <- cluster$cnames_quanti
   cnames_quali <- cluster$cnames_quali
   description <- list()
   # suppress the two first character of a string
-  cut_1 <- function(s) { Reduce(paste0,sapply(strsplit(s,""), identity)[-c(1,2)])} 
-  
+  cut_1 <- function(s) { Reduce(paste0,sapply(strsplit(s,""), identity)[-c(1,2)])}
+
   for (j in 1:length(leaves))
   {
     description[[j]] <- character()
@@ -125,14 +125,14 @@ make_description <- function(leaves,cluster)
         select_mod <- mod_quali[[x$cut_ind]][x$val]
         select_str <- Reduce(function (x,y) {paste(x,y, sep=",")}, select_mod)
         symbol_in <- " = "
-        sentence <- paste0(cnames_quali[[x$cut_ind]], symbol_in, "{", select_str, "}")  
-      } 
+        sentence <- paste0(cnames_quali[[x$cut_ind]], symbol_in, "{", select_str, "}")
+      }
       description[j] <- paste(description[[j]], sentence, sep=' , ')
-    }    
+    }
     description[j] <- cut_1(description[[j]])
   }
   return(description)
-} 
+}
 
 
 
@@ -146,20 +146,20 @@ make_description <- function(leaves,cluster)
 #' @export
 
 make_sentences <- function(cluster, node) {
-  
+
   mod_quali <- cluster$mod_quali
   cnames_quanti <- cluster$cnames_quanti
   cnames_quali <- cluster$cnames_quali
   index <-  node$v$cut_ind
-  
+
   if (node$v$cut_val$type =="quanti") {
     #value <-  node$v$cut_val$value
     #cut_val <- value * cluster$sig_quanti[index] + cluster$mu_quanti[index]
     cut_val <-  node$v$cut_val$value
     sentence <- paste0(cnames_quanti[index], " < ", round(cut_val, digits=2))
     sentence_comp <- paste0(cnames_quanti[index], " >= ", round(cut_val, digits=2))
-  }   
-  
+  }
+
   else if (node$v$cut_val$type =="quali") {
     value <-  node$v$cut_val$value
     select_mod <- mod_quali[[index]][value$bipart]
@@ -168,24 +168,24 @@ make_sentences <- function(cluster, node) {
     select_comp_str <- Reduce(function (x,y) {paste(x,y, sep=",")}, select_comp)
     #symbol_in <- " \u220A "
     symbol_in <- " = "
-    sentence <- paste0(cnames_quali[[index]], symbol_in, "{", select_str, "}") 
-    sentence_comp <- paste0(cnames_quali[[index]], symbol_in,"{", select_comp_str, "}") 
+    sentence <- paste0(cnames_quali[[index]], symbol_in, "{", select_str, "}")
+    sentence_comp <- paste0(cnames_quali[[index]], symbol_in,"{", select_comp_str, "}")
   }
-  
-  else { 
-    stop("unknown cut_val$type : must be quali or quanti") 
+
+  else {
+    stop("unknown cut_val$type : must be quali or quanti")
   }
   return(list(left = sentence, right = sentence_comp))
 }
 
 #' @title Binary transformation of categorical data
-#' @description Organizes categorical data into an indicator matrix and gives a description of the categorical variables 
+#' @description Organizes categorical data into an indicator matrix and gives a description of the categorical variables
 #' @keywords internal
 #' @param frame a data frame with n rows and p categorical colums (columns are of class factor)
-#' @return \item{Y}{an indicator matrix with n rows and m columns where m is the total number of categories} 
+#' @return \item{Y}{an indicator matrix with n rows and m columns where m is the total number of categories}
 #' @return \item{vec_quali}{a vector of size p containing the number of categories of each variable}
 #' @return \item{mod_quali}{a list with the set of categories of each variable}
-#' @export 
+#' @export
 format_cat_data <- function(frame) {
   #X<-as.matrix(frame)
   X <- frame
@@ -194,7 +194,7 @@ format_cat_data <- function(frame) {
   #mod_quali <- apply(X, 2, unique)
   mod_quali <- lapply(X,function(x){levels(x)})
   vec_quali <- sapply(mod_quali, length)
-  nb_cols <- sum(vec_quali)  
+  nb_cols <- sum(vec_quali)
   offsets <- c(0, cumsum(vec_quali))
   # builds the disjonctif table
   Y<-matrix(0, nrow(X), nb_cols)
@@ -219,7 +219,7 @@ gsvd <- function(A, M, N) {
     stop('M must be a vector')
   z_t <- diag(sqrt(N)) %*% A %*% diag(sqrt(M))
   sig_t <- svd(z_t)
-  u <- diag(1./sqrt(N)) %*% sig_t$u 
+  u <- diag(1./sqrt(N)) %*% sig_t$u
   v <- diag(1./sqrt(M)) %*% sig_t$v
   return (list(u = u, v = v, d = sig_t$d))
 }
@@ -253,20 +253,20 @@ inert_1D <- function (Z, compo, indices, w = rep(1. / nrow(Z), nrow(Z)), D = rep
 #' @param indices indices vectors of the first class (\eqn{C_1})
 #' @param indices_c indices vectors of the second class (\eqn{C_2})
 #' @param w weights vectors
-#' @param D diagonal distance matrix vector 
+#' @param D diagonal distance matrix vector
 #' @keywords internal
 #' @export
-#' 
-between_cluster_inert_1D <- function (Z, compo, indices, indices_c, w = rep(1. / nrow(Z), nrow(Z)), D = rep(1, ncol(Z))) 
-{ 
+#'
+between_cluster_inert_1D <- function (Z, compo, indices, indices_c, w = rep(1. / nrow(Z), nrow(Z)), D = rep(1, ncol(Z)))
+{
   # checks there is not a empty class
   if (is.null(indices) | is.null(indices_c)) stop('between_cluster_inert : one of the two cluster is empty!')
   mu <- sum(w[indices])
   g <- sum(w[indices] * Z[indices, compo]) / mu
-  
+
   mu_c <- sum(w[indices_c])
-  g_c <- sum(w[indices_c] * Z[indices_c, compo]) / mu_c 
-  
+  g_c <- sum(w[indices_c] * Z[indices_c, compo]) / mu_c
+
   inert <- D[compo] * (g - g_c)^2
   inert <- (mu_c * mu) / (mu_c + mu) * inert
   return(inert)
@@ -278,12 +278,12 @@ between_cluster_inert_1D <- function (Z, compo, indices, indices_c, w = rep(1. /
 #' @param indices indices vectors of the first class (\eqn{C_1})
 #' @param indices_c indices vectors of the second class (\eqn{C_2})
 #' @param w weights vector
-#' @param D diagonal distance matrix vector 
+#' @param D diagonal distance matrix vector
 #' @keywords internal
 #' @export
 
-between_cluster_inert <- function (Z, indices, indices_c, w = rep(1. / nrow(Z), nrow(Z)), D = rep(1, ncol(Z))) 
-{ 
+between_cluster_inert <- function (Z, indices, indices_c, w = rep(1. / nrow(Z), nrow(Z)), D = rep(1, ncol(Z)))
+{
   # checks there is not a empty class
   if (is.null(indices) | is.null(indices_c)) stop('between_cluster_inert : one of the two cluster is empty!')
   if (length(indices) > 1) {
@@ -309,7 +309,7 @@ between_cluster_inert <- function (Z, indices, indices_c, w = rep(1. / nrow(Z), 
 
 
 #' @title Computes the cut values for a numerical variable
-#' @description Function which computes the cut values of the binary questions defined 
+#' @description Function which computes the cut values of the binary questions defined
 #' with a numerical variables (a column of the data matrix).
 #' @param col the column vector in the matrix
 #' @return a vector of cut values
