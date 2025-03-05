564e8d1ec1f9161784806d79b788402ae7fd477f
borishejblum <boris.hejblum@gmail.com>
2025-03-05 11:24:23 +0100

    trying to trigger run on GHA

--- R/cutreediv.R
+++ R/cutreediv.R
@@ -1,11 +1,12 @@
-#' @title Cut the tree
-#' @description 
-#' This function cuts the tree into several cluters by specifying the desired number of clusters. 
+#' Cut a divisive tree
+#'
+#' This function cuts the tree into several cluters by specifying the desired number of clusters.
+#'
 #' @param tree the divclust object
 #' @param K an integer with the desired number of clusters.
 #' @return \item{clusters}{the list of observations in each  cluster}
 #' @return \item{description}{the monothetic description of each cluster}
-#' @return \item{which_cluster}{a vector of integers indicating the cluster of each observation} 
+#' @return \item{which_cluster}{a vector of integers indicating the cluster of each observation}
 #' @return \item{B}{the proportion of inertia explained by the partition (between-cluster inertia/total inertia)}
 #' @return \item{leaves}{an internal list of \strong{leaves}}
 #' @export
@@ -14,20 +15,20 @@
 #' tree <- divclust(protein) # full clustering
 #' p_5 <- cutreediv(tree,K=5) # partition in 5 clusters
 #' p_5
-#' 
+#'
 #' data(dogs) # pure qualitative data
 #' tree <- divclust(dogs) # full clustering
 #' p_4 <- cutreediv(tree,K=4) # partition in 4 clusters
 
-#' data(wine) # mixed data 
+#' data(wine) # mixed data
 #' data <- wine[,1:29]
 #' tree <- divclust(data) # full clustering
-#' p_4 <- cutreediv(tree, 4) # 
-#' 
+#' p_4 <- cutreediv(tree, 4) #
+#'
 
-cutreediv <- function(tree,K) { 
+cutreediv <- function(tree,K) {
   cluster <- tree
-  if (!inherits(cluster, "divclust")) 
+  if (!inherits(cluster, "divclust"))
     stop("use only with \"divclust\" objects")
   if (!(K %in% 1:(length(cluster$clusters)-1))) stop(paste("K must be an integer between 0 and",length(cluster$clusters)-1))
   l <- list()
@@ -35,23 +36,23 @@ cutreediv <- function(tree,K) {
   node_init <- cluster$tree
   inert_init <- node_init$v$inert
   fifo_add(f, list(node = node_init, path = NULL, inert = inert_init, stage = 1))
-  
+
   while ( !fifo_is_empty(f)) {
     z <- fifo_remove(f)
     node <- z$node
     path <- z$path
     inertie <- z$inert
     stages <- z$stage
-    
+
     sentence <- make_path(node, path)
     path_l <- sentence$l
     path_r <- sentence$r
-    
+
     inert_l <- node$l$v$inert
     inert_r <- node$r$v$inert
-    
-    if (node$l$v$inert <= cluster$height[K]) {   
-      l[[length(l) + 1]] <-  list(class = node$v$A_l, path = path_l, inert = inertie, stage = stages) 
+
+    if (node$l$v$inert <= cluster$height[K]) {
+      l[[length(l) + 1]] <-  list(class = node$v$A_l, path = path_l, inert = inertie, stage = stages)
     } else {
       fifo_add(f, list(node = node$l, path = path_l, inert = append(inertie, inert_l), stage = stages + 1))
     }
@@ -59,17 +60,17 @@ cutreediv <- function(tree,K) {
       l[[length(l) + 1]]  <- list(class = node$v$A_l_c, path = path_r, inert = inertie, stage = stages)
     } else {
       fifo_add(f, list(node = node$r, path = path_r, inert = append(inertie, inert_r), stage = stages + 1))
-    }  
+    }
   }
-  c <- rep(0, length(l)) 
-  k <- 1 
-  for (i in l) { 
+  c <- rep(0, length(l))
+  k <- 1
+  for (i in l) {
     for (j in i$class) {
-      c[j] <- k 
-    } 
-    k <- k + 1 
+      c[j] <- k
+    }
+    k <- k + 1
   }
-  
+
   part <- list()
   part$description <- make_description(l,cluster)
   names(part$description) <- paste("C", 1:length(l), sep = "")
